c979efcfa4a74eeedfe74d566ca34b30dab71ac0
Daniel Grieve <dnlgrv@hey.com>
2023-08-26 21:14:47 +0100

    Remove active class on nav buttons

--- app/javascript/controllers/layout_controller.js
+++ app/javascript/controllers/layout_controller.js
@@ -1,16 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static classes = ["active", "hide"]
+  static classes = ["hide"]
   static targets = ["instructions"]
 
-  toggleInstructions(event) {
+  toggleInstructions() {
     this.instructionsTarget.classList.toggle(this.hideClass)
-
-    if (this.instructionsTarget.classList.contains(this.hideClass)) {
-      event.currentTarget.classList.remove(...this.activeClasses)
-    } else {
-      event.currentTarget.classList.add(...this.activeClasses)
-    }
   }
 }
