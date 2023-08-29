3334aa3b37c98f317d2b8dec89fb4d876b2ce5be
Daniel Grieve <dnlgrv@hey.com>
2023-08-29 14:25:50 +0100

    Dynamically set turbo frame id

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,9 +1,17 @@
 class FilesController < ApplicationController
+  before_action :set_turbo_frame_id
+
   def show
     exercise = Exercise.find_by_slug(params[:exercise_id])
 
     @file = exercise.related_files_as_files.find do |f|
       f.to_param == params[:md5]
     end
+  end
+
+  private
+
+  def set_turbo_frame_id
+    @turbo_frame_id = turbo_frame_request_id
   end
 end
