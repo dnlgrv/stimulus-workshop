e73f2c54445230c90d382d8e6aad8d931af28cec
Daniel Grieve <dnlgrv@hey.com>
2023-08-31 13:29:46 +0100

    totally tabular solution

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -2,6 +2,35 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tabs"
 export default class extends Controller {
-  connect() {
+  static targets = ["tab", "section"]
+  static values = {
+    selectedIndex: { type: Number, default: 0 }
+  }
+
+  select(event) {
+    const { index } = event.params
+    this.selectedIndexValue = index
+  }
+
+  selectedIndexValueChanged(newIndex, oldIndex) {
+    if (oldIndex !== undefined) {
+      this.deselectTab(oldIndex)
+    }
+
+    if (newIndex !== undefined) {
+      this.selectTab(newIndex)
+    }
+  }
+
+  selectTab(index) {
+    this.tabTargets[index].classList.add("bg-white")
+    this.sectionTargets[index].classList.remove("hidden")
+    this.sectionTargets[index].classList.add("flex")
+  }
+
+  deselectTab(index) {
+    this.tabTargets[index].classList.remove("bg-white")
+    this.sectionTargets[index].classList.add("hidden")
+    this.sectionTargets[index].classList.remove("flex")
   }
 }
