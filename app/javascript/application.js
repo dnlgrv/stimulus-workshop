a146205a5c71c9b8ff2ad7bbfad99c13d9410807
Daniel Grieve <dnlgrv@hey.com>
2023-08-31 13:32:25 +0100

    turbo tastic solution

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import { StreamActions } from "@hotwired/turbo"
+
+StreamActions.clear = function () {
+  this.targetElements.forEach(el => el.value = "")
+}
